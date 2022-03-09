849df04668b264d9e831dbd32d5ad5e8472ba8d6
fegloff <fegloff@gmail.com>
2022-03-08 23:38:40 -0500

    add metamask connection

--- src/components/metamask-button/metamask-button.component.jsx
+++ src/components/metamask-button/metamask-button.component.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import './metamask-button.styles.scss';
 import '../../assets/fonts/LeagueGothic-Italic.otf';
 
+import { connect } from 'react-redux';
+import { setCurrentWallet } from '../../redux/wallet/wallet.actions';
 
 class MetamaskButton extends React.Component {
 
- /* async componentDidMount() {
+ async componentDidMount() {
     const { setCurrentWallet } = this.props;
 
     const { ethereum } = window;
@@ -41,14 +42,23 @@ class MetamaskButton extends React.Component {
         console.log(e);
       }
     }
-  }*/
+  }
   
   render() {
-    const text = 'CONNECT \n WALLET';
     return (
-      <button className='button'>{text}</button>
+      <button className='button' onClick={this.connectWallet}>
+        {this.props.currentWallet ? 'Connected' : 'Connect \n Wallet'}
+      </button>
     );
   }
 }
 
-export default MetamaskButton;
+const mapStateToProps = ({ wallet }) => ({
+  currentWallet: wallet.currentWallet
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentWallet: wallet => dispatch(setCurrentWallet(wallet))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MetamaskButton);
