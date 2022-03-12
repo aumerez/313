3a6bf426b556f55123e16a07462e507a725638c5
aumerez <aumerez@gmail.com>
2022-03-12 16:07:43 -0500

    Cambios del mint

--- src/pages/mintpage/mintpage.component.jsx
+++ src/pages/mintpage/mintpage.component.jsx
@@ -3,19 +3,31 @@ import React from 'react';
 import ImageMint from '../../assets/imgs/Illustration_Mint-01.png';
 import MintButton from '../../components/mint-button/mint-button.component';
 
+import backgroundImageMint from '../../assets/imgs/background_mint-01.png'
+
 import './mintpage.styles.scss';
 
 const MintPage = () => (
-    <div className='mints'>
-        <img src={ImageMint} id='bg1' />
-        <div className='text-wrapper'>
-            <h1>MINT A</h1>
-            <h1>DETROIT</h1>
-            <h1>SOCIALITE</h1>
-            <MintButton />
-            {/*<button className="btn">CLICK HERE</button>*/}
+    <div className='page'>
+        <img className='bg-image' src={backgroundImageMint} alt='' />
+        <div className='container'>
+            <div >
+                <img className='image' src={ImageMint} alt='' />
+            </div>
+            <div className='text-wrapper'>
+                <h1>MINT A</h1>
+                <h1>DETROIT</h1>
+                <h1>SOCIALITE</h1>
+            </div>
+
+
+            <div className='btn'>
+                <MintButton />
+            </div>
+
+
         </div>
     </div>
-  );
+);
 
-  export default MintPage;
+export default MintPage;
