d57b7d4645c4e73da1cbeb6bd0e658233b0f0be4
fegloff <fegloff@gmail.com>
2022-03-16 22:19:43 -0500

    add sm-text for pages home and mint

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Ticker from 'react-ticker'
 import './homepage.styles.scss';
 import Image from 'react-bootstrap/Image';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/fonts/LeagueGothic-Italic.otf';
@@ -10,28 +12,42 @@ import homeImage from '../../assets/imgs/Illustration-02_sm.png';
 import backgroundImage from '../../assets/imgs/Background_Orange_sm.png';
 import scroll from '../../assets/imgs/ScrollBar-01.png';
 
-const HomePage = () => (
-  <div>
-    <div className="home-page">
-      <img src= {backgroundImage} className='home-bg-image' alt=""/>
-      
-      <div >
-        <Image src={homeImage}  className='home-img'/> 
-        {/*<img src= {homeImage} className='img' alt="" />*/}
+const HomePage = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
+  return (
+    <div>
+      <div className="home-page">
+        <img src= {backgroundImage} className='home-bg-image' alt=""/>
+        
+        <div >
+          <Image src={homeImage}  className='home-img'/> 
+          {/*<img src= {homeImage} className='img' alt="" />*/}
+
+        </div>
+        
       </div>
-      
+      { !matches ? (
+        <div >
+          <h1 className="sm-text-home">
+          LET'S BUILD AND<br />MAKE CHANGE TOGETHER.<br />
+          ROOTED IN DETROIT,<br />MISSION WORLDWIDE
+          </h1>
+
+        </div>
+      ) : null }
+      <div className='bottonBand fixed-bottom' 
+        style={{ backgroundImage: `url(${scroll})`, marginBottom: '80px'}}>
+        <Ticker>{({ index }) => (
+          <>
+            <h1 className='textBand'>BUILDING IN A CITY WE LOVE.  </h1>
+            <img src="www.my-image-source.com/" alt=""/>
+          </>)}
+        </Ticker>
+      </div>
     </div>
-    <div className='bottonBand fixed-bottom' 
-      style={{ backgroundImage: `url(${scroll})`, marginBottom: '80px'}}>
-      <Ticker>{({ index }) => (
-        <>
-          <h1 className='textBand'>BUILDING IN A CITY WE LOVE.  </h1>
-          <img src="www.my-image-source.com/" alt=""/>
-        </>)}
-      </Ticker>
-    </div>
-  </div>
-);
+  );
+}
 
 export default HomePage;
--- src/pages/mint-page/mint-page.component.jsx
+++ src/pages/mint-page/mint-page.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Image from 'react-bootstrap/Image';
 import MintButton from '../../components/mint-button/mint-button.component';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/fonts/LeagueGothic-Italic.otf';
@@ -11,23 +13,38 @@ import mintImage from '../../assets/imgs/Illustration_Mint-01_Text2_sm.png';
 import backgroundImage from "../../assets/imgs/background_mint-01_sm.png";
 
 
-const MintPage = () => (
-  <div>
-    <div className="mint-page">
-      <img src= {backgroundImage} className='mint-bg-image' alt=""/>
-      
-      <div >
-        <Image src={mintImage} position='sticky' className='mint-img'/> 
-        {/*<img src= {homeImage} className='img' alt="" />*/}
+const MintPage = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
+  return (
+    <div>
+      <div className="mint-page">
+        <img src= {backgroundImage} className='mint-bg-image' alt=""/>
+        
+        <div >
+          <Image src={mintImage} position='sticky' className='mint-img'/> 
+          {/*<img src= {homeImage} className='img' alt="" />*/}
+
+        </div>
+        
       </div>
-      
+      { !matches ? (
+          <div >
+            <h1 className="sm-text-mint">
+            CONNECT YOUR WALLET.<br />
+            MINT YOUR SOCIALITE.<br />
+            SOCIALIZE.
+            </h1>
+
+          </div>
+      ) : null }
+      <div className='fixed-bottom d-flex align-items-center justify-content-center text-center' 
+        style={{ marginBottom: '100px'}}>
+        <MintButton />
+      </div>
     </div>
-    <div className='fixed-bottom d-flex align-items-center justify-content-center text-center' 
-      style={{ marginBottom: '100px'}}>
-      <MintButton />
-    </div>
-  </div>
-);
+  );
+}
 
 export default MintPage;
